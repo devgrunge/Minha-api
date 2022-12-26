692653eb55f9f202cd51a4debde9b70275641477
alexanderJr <alexmarcjr21@gmail.com>
2022-12-26 18:44:54 +0000

    feat: auth with jwt and auth midlleware

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: "c4ec3033a06f5f26e9d493440381a5ba456a245845e1603ed2e2675a3fee284d",
+  expireIn: "6h",
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
-const { Router } = require('express');
-
-const UserController = require('./app/Controllers/UserController');
+const { Router } = require("express");
+const LoginController = require("./app/Controllers/LoginController");
+const AuthMidlleware = require("./app/Midlewares/AuthMidleware");
+const UserController = require("./app/Controllers/UserController");
 
 const routes = new Router();
 
-routes.post("/user", UserController.store );
-routes.get("/user", UserController.show );
-
+routes.post("/user", UserController.store);
+routes.get("/user", AuthMidlleware, UserController.show);
+routes.post("/login", LoginController.index);
 
 module.exports = routes;
